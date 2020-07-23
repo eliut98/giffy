7b1a6d2d50919239e42720460af8fd2e6caa5b52
Roberth González <roberthgon18@gmail.com>
2020-07-23 20:02:40 +0200

    Search gif

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ListOfGifs from "./components/ListOfGifs";
-import { Route } from "wouter";
+import { Route, useLocation } from "wouter";
 
 import "./styles/app.css";
 
 export default function App() {
+  const [location, setLocation] = useLocation();
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setLocation(`/gif/${query}`);
+  };
+
   return (
     <div className="app">
-      <div className="search">
-        <input type="search" placeholder="Search  all the gifs you want" />
+      <form className="search" onSubmit={(e) => handleSearch(e)}>
+        <input
+          type="search"
+          placeholder="Search  all the gifs you want"
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button>Search</button>
-      </div>
+      </form>
       <Route path="/gif/:query" component={ListOfGifs} />
     </div>
   );
