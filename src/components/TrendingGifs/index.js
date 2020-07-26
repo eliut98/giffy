ea1991cc45df4026c54b3c381ffa53fb5772c74d
Roberth González <roberthgon18@gmail.com>
2020-07-26 12:42:49 +0200

    Lazyload trending search

--- src/components/TrendingGifs/index.js
+++ src/components/TrendingGifs/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import getTrendingGifs from "services/getTrendingGifs";
 import Category from "components/Category";
 
@@ -7,9 +7,30 @@ const TrendingGifs = () => {
 
   useEffect(() => {
     getTrendingGifs().then(setTrends);
-  },[])
+  }, []);
 
   return <Category name="Tendencias" options={trends} />;
 };
 
-export default TrendingGifs;
+export default function LazyTrending() {
+  const [show, setShow] = useState(false);
+  const elementRef = useRef();
+
+  useEffect(() => {
+    const onView = (entries) => {
+      const element = entries[0];
+      if (element.isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    };
+
+    const observer = new IntersectionObserver(onView, { rootMargin: "100px" });
+
+    observer.observe(elementRef.current);
+
+    return observer.disconnect();
+  });
+
+  return <div ref={elementRef}>{show ? <TrendingGifs /> : null}</div>;
+}
